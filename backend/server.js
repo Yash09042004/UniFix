c99e74192abe042f22b79583c6dfdaefc6aaed8a
wlug <osd.wlug@gmail.com>
2025-04-02 16:52:19 +0530

    updated new

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
     res.send('UniFix API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
